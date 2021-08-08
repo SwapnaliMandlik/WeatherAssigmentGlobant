2d0088d12602d3b50034f41651817fe8cf553131
SwapnaliMandlik <88276407+SwapnaliMandlik@users.noreply.github.com>
2021-08-08 19:44:39 +0530

    commit the details..

---
+++ pods/Target Support Files/Pods-WeatherAssigment-WeatherAssigmentUITests/Pods-WeatherAssigment-WeatherAssigmentUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WeatherAssigment_WeatherAssigmentUITests : NSObject
+@end
+@implementation PodsDummy_Pods_WeatherAssigment_WeatherAssigmentUITests
+@end
---
+++ pods/Target Support Files/Pods-WeatherAssigmentTests/Pods-WeatherAssigmentTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WeatherAssigmentTests : NSObject
+@end
+@implementation PodsDummy_Pods_WeatherAssigmentTests
+@end
